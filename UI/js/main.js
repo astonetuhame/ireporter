af84f8c2879d63ee907fd10d52bff98bb22f8eec
Taent <taentastone@gmail.com>
2018-11-24 11:52:24 -0800

    Update Index Page

---
+++ UI/js/main.js
@@ -0,0 +1,19 @@
+function confirmDelete(){
+    confirm('Are you sure you want to delete this record?');
+}
+
+function confirmRecord(){
+    confirm('Are you sure you want to submit this record?');
+}
+
+function confirmEdit(){
+    confirm('Are you sure you want to submit this edited record?');
+}
+
+function confirmAccountCreation(){
+    confirm('Are you sure you want to submit this data?');
+}
+
+function changeStatus(){
+    alert('Status changed!');
+}
